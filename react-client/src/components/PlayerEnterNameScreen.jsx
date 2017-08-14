1e8a75b269174a4967dba0d03b9407bb256ecbbf
Yang Lu <me@yang.lu>
2017-08-14 11:14:02 -0700

    fixed weird rebase issues

--- react-client/src/components/PlayerEnterNameScreen.jsx
+++ react-client/src/components/PlayerEnterNameScreen.jsx
@@ -5,42 +5,19 @@ class PlayerEnterNameScreen extends React.Component {
   constructor(props) {
     super(props);
 
-  }
+  };
 
   render() {
 
     return (
       <div>
 
-        <h2> Join a Game of Defintely Not Avalon </h2>
-
-
-      I do not presently know how to deal with the html form
-
-     
-<form >
-  <input type="text" name="Token" value="Enter Access Token"/>
-
-  <input type="text" name="Name" value="Enter name"/>
-
-  <input type="submit" value="Submit"/>
-</form> 
-
-      
-
-
-        <button onClick={this.props.backButtonClickHandler}>
-        {'Back'}
-        </button>
-
-        <button onClick={this.props.joinButtonClickHandler}>
-        {'Join'}
-        </button>
-
+        <h2> Please Enter your Name </h2>
 
       </div>
-      )
-  }
-}
+    )
+  };
+
+};
 
 export default PlayerEnterNameScreen;
